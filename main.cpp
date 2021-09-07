a643a15e0090702c4877b13f46e85bdc9e2dce04
jacekpiotrowskiRCEZ <76050734+jacekpiotrowskiRCEZ@users.noreply.github.com>
2021-09-07 13:32:56 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+float kwotaBrutto;	
+
+cout<< "Podaj kwotê Brutto"<<endl;
+cin>>kwotaBrutto;
+
+cout<<" kwota podatku wynosi \t"<<kwotaBrutto*0.19<<endl;
+
+cout<<" kwota netto wynosi \t"<<kwotaBrutto -kwotaBrutto*0.19<<endl;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
